e7a2c05b2e4e1eb033d8ee19f404ae722c6ede81
$chouty <aschou101229@gmail.com>
2024-09-06 14:27:32 -0500

    Updated setEventListeners

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -3,6 +3,7 @@ import Popup from "./Popup.js";
 export class PopupWithConfirmation extends Popup{
   constructor(popupSelector){
     super(popupSelector);
+    this._confirmButton = this._popupElement.querySelector('.modal__button');
   }
 
   setSubmitFunction(submitFunction){
@@ -10,8 +11,9 @@ export class PopupWithConfirmation extends Popup{
   }
 
   setEventListeners() {
-    super.setEventListeners();
-    this._handleConfirm;
+    super.setEventListeners();''
+    this._confirmButton.addEventListener('click', () =>{
+      this._handleConfirm();
+    })
     }
-    
   }
